f925d11251fc18e81cbb2b25484ebe7886ad80b8
derekgelveza <gel24002@byui.edu>
2025-09-30 09:25:35 -0600

    database set up

--- app/database.py
+++ app/database.py
@@ -1,12 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#Database URL (This is where all the users info will exsist)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./nutrition.db"
 
-engine = create_engine(
+#connection for the database
+engine = create_engine (
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+#autocommit-False -> changes are not saved unil you say commit
+#autoflush=False -> stops automatic sending of changes until you're ready
+#bind=engine ties the session to database (sqlconnection)
+#Hanlde for databse. Every request is its own session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+#allows for python to map to sql tables
+Base = declarative_base ()
+
+#database.py connectos to pythong and sqlite
+#other fiels define the tables using Base
+#this will allow users to store weight goals, meals, progress withing the database safely
--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, String, Float
-from .database import Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DataTime #foreignKey creates link between tables
+from sqlalchemy.orm import relationship #relationship allows to move between tables
+from .database import Base #imports base from .database and tells sqlalchemy that those classes should be tables
+import datetime
 
 class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    __tablename__ = "users" #determines name of table in databes
+
+    id = Column (Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
     age = Column(Integer)
-    sex = Column(String)
-    height = Column(Float)
-    weight = Column(Float)
-    goal = Column(String)
+    sex = Column(String) #male, female, other, prefer not to say
+    height = Column(float) #in cm
+    weight = Column(float) #in kg
+    goal = Column(String) #lose, gain, maintain
